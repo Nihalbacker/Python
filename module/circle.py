8f3f9f5b13ee7f2131a0c81c82fc0d6335e4ab3d
Nihal Backer <nihalbackerak@gmail.com>
2023-01-18 15:53:53 +0530

    Add files via upload

---
+++ module/circle.py
@@ -0,0 +1,4 @@
+def circlearea(r):
+    print("Area=",3.14*r*r)
+def circleper(r):
+    print("Perimeter=",2*3.14*r*r)
---
+++ module/rectangle.py
@@ -0,0 +1,4 @@
+def rectarea(l,b):
+    print("Area=",l*b)
+def rectper(l,b):
+    print("Perimeter=",2*(l+b))
